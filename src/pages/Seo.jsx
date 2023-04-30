6bec185108db61185d2836e8d659b85bbd4b79a3
raul far <raulfarllobell@gmail.com>
2023-04-30 02:48:12 +0200

    update

--- src/pages/Seo.jsx
+++ src/pages/Seo.jsx
@@ -8,9 +8,20 @@ import {
 import { SnackbarUtilities } from '@/utilities';
 
 
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import List from '@mui/material/List';
+import ListSubheader from '@mui/material/ListSubheader';
+
+
+
+
 const Seo = () => {
 
     const [url, setUrl] = useState("");
+    const [data, setData] = useState(null);
+    const [headings, setHeadings] = useState(undefined);
+
 
     const handleChange = (e) => {
         setUrl(e.target.value);
@@ -23,7 +34,7 @@ const Seo = () => {
 
 
         (async () => {
-            if(regex .test(url)) {
+            if (regex.test(url)) {
                 const encodedParams = new URLSearchParams();
                 encodedParams.set('url', url);
 
@@ -41,15 +52,40 @@ const Seo = () => {
                 try {
                     const response = await axios.request(options);
                     console.log(response.data);
+                    let a = [];
+                    for (let i = 0; i <= 6; i++) {
+                        if (response.data[`h${i}`] != undefined) {
+                            if (response.data[`h${i}`].length = 1) {
+                                // a = { [`h${i}`]: `${response.data[`h${i}`]}` };
+
+
+                                a.push({ [`h${i}`]: `${response.data[`h${i}`]}` });
+
+
+                            }
+
+
+
+
+                        }
+                    }
+                    //   console.log({a})
+                    setHeadings(a);
+
+                    // console.log({ headings })
+                    // console.log(headings.length)
+
+
+                    setData(response.data)
                 } catch (error) {
                     console.error(error);
                 }
-            }else{
-                console.log("no es una url")
+            } else {
                 SnackbarUtilities.error("Url inválida");
             }
         })()
     };
+
 
 
 
@@ -90,28 +126,58 @@ const Seo = () => {
 
 
 
+            {/* {console.log(headings?.length)} */}
+            {console.log(headings)}
+
+            {headings?.length > 0 ?
+                <>
+                    {console.log({ headings })}
+                    <h2 className="text-2xl font-bold  mt-10 mb-5">h2</h2>
+                    < List
+                        sx={{
+                            width: '100%',
+                            maxWidth: 360,
+                            position: 'relative',
+                            overflow: 'auto',
+                            maxHeight: 700,
+                            '& ul': { padding: 0 },
+                            bgcolor: '#cbdaf3',
+                        }}
+                        subheader={<li />}
+                    >
+                        {["h1", "h2", "h3", "h4", "h5"].map((titleHeading) => (
+                            <li key={`section-${titleHeading}`}>
+                                <ul>
+                                    {console.log({ headings })}
+
+                                    {headings.map(item => {
+                                        { console.log({item}) }
+
+                                        { console.log(item.h1) }
+                                        { console.log("s", item.titleHeading) }
+
+
+                                        item?.titleHeading&&
+                                            <ListItem key={`item-${titleHeading}-${item}`}>
+                                                <ListItemText primary={`Items ${item.titleHeading}`} />
+                                            </ListItem>
+
+                                    })}
+
+
+                                </ul>
+                            </li>
+                        ))}
+                    </List>
+
+                </>
+
+                : null}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        </section>
+        </section >
     );
 };
 
